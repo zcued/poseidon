8ebc8e62791c6838fa84b8997c16e4911bfa9601
zhlooking <zhlooking@users.noreply.github.com>
2019-01-18 16:49:47 +0800

    FIX: dropdown controlled bug (#41)

--- packages/dropdown/src/index.js
+++ packages/dropdown/src/index.js
@@ -61,6 +61,10 @@ function Dropdown(props) {
 
   let timer = null
 
+  if (isControl && props.isOpen !== isOpen) {
+    setIsOpen(props.isOpen)
+  }
+
   const handleClick = e => {
     if (trigger === 'click') {
       if (!isControl) {
@@ -95,9 +99,14 @@ function Dropdown(props) {
     }
   }
 
-  const handleClickOutSide = () => {
+  const handleClickOutSide = e => {
+    if (!isOpen) {
+      return
+    }
     if (!isControl) {
       setIsOpen(false)
+    } else {
+      onToggle(e)
     }
   }
 
