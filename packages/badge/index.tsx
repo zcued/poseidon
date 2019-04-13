db558bdb5d7042c8444aee0c1fa3c5e4f59edbf1
huangdehao <vq0599@outlook.com>
2019-04-13 14:43:59 +0800

    ✨ Add props 'visible' for badge component

--- packages/badge/index.tsx
+++ packages/badge/index.tsx
@@ -17,6 +17,7 @@ export interface BadgeProps {
   className?: string
   children?: React.ReactNode
   count?: number | string
+  visible?: boolean
 }
 
 const Badge = styled(BaseBadge)`
@@ -26,6 +27,7 @@ const Badge = styled(BaseBadge)`
   sup {
     position: absolute;
     padding: 0 3px;
+    visibility: ${({ visible }) => visible ? 'visible' : 'hidden'};
     top: ${({ offset }) =>
       Array.isArray(offset) && offset[0] ? `${offset[0]}px` : 0};
     right: ${({ offset }) =>
@@ -48,6 +50,7 @@ Badge.displayName = 'Badge'
 
 Badge.defaultProps = {
   theme,
+  visible: true,
   dot: false
 }
 
